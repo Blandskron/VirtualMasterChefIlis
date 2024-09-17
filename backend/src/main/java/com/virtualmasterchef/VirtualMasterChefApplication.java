e3c279bc9aca8d69c5f28b525932d0f35f120818
blandskron <bastianlandskronfreelancer@gmail.com>
2024-09-17 17:16:08 -0300

    backend

---
+++ backend/src/main/java/com/virtualmasterchef/VirtualMasterChefApplication.java
@@ -0,0 +1,12 @@
+package com.virtualmasterchef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualMasterChefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualMasterChefApplication.class, args);
+    }
+}
