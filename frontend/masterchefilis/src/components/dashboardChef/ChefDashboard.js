ba2ac02b2a97a2cc6071fe139fc373d5b6b28b26
blandskron <bastianlandskronfreelancer@gmail.com>
2024-09-19 22:21:26 -0300

    update

--- frontend/masterchefilis/src/components/dashboardChef/ChefDashboard.js
+++ frontend/masterchefilis/src/components/dashboardChef/ChefDashboard.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './ChefDashboard.css';
 
 const ChefDashboard = () => {
   const [recipes, setRecipes] = useState([]);
